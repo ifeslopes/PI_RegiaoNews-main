c20c83c712816d1deda9df555a20bd09483a746b
ifeslopes <ifes.lopes@gmail.com>
2021-12-01 12:27:59 -0300

    criando classe noticias e testando get set

---
+++ src/entidades/Noticias.php
@@ -0,0 +1,91 @@
+<?php
+class Noticias
+{
+    private $idnoticia;
+    private $titulo;
+    private $textoDestaque;
+    private $textoCompleto;
+    private $foto;
+    private $data;
+    private $idadminApioadoFK;
+
+    function __construct(
+        $titulo,
+        $textoDestaque,
+        $textoCompleto,
+        $foto,
+        $data,
+        $idadminApioadoFK
+    ) {
+        $this->titulo = $titulo;
+        $this->textoDestaque = $textoDestaque;
+        $this->textoCompleto = $textoCompleto;
+        $this->foto = $foto;
+        $this->data = $data;
+        $this->idadminApioadoFK = $idadminApioadoFK;
+    }
+
+    public function getIdnoticia()
+    {
+        return $this->idnoticia;
+    }
+
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getTextoDestaque()
+    {
+        return $this->textoDestaque;
+    }
+
+    public function setTextoDestaque($textoDestaque)
+    {
+        $this->textoDestaque = $textoDestaque;
+
+        return $this;
+    }
+
+    public function getTextoCompleto()
+    {
+        return $this->textoCompleto;
+    }
+
+    public function setTextoCompleto($textoCompleto)
+    {
+        $this->textoCompleto = $textoCompleto;
+
+        return $this;
+    }
+
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function getIdadminApioadoFK()
+    {
+        return $this->idadminApioadoFK;
+    }
+}
+?>
--- src/index1.php
+++ src/index1.php
@@ -1,5 +1,6 @@
 <?php
 include_once "entidades/AdminApoiado.php";
+include_once "entidades/Noticias.php";
 $pessoa = new AdminApoiado(
     "leonardo",
     "1234",
@@ -8,12 +9,33 @@ $pessoa = new AdminApoiado(
     "estutande de informatica",
     "teste.jpg"
 );
+$noticia = new Noticias(
+    "primeira notica",
+    "primeria",
+    "texto completo do primiro",
+    "foto.jpg",
+    "1/12/2021",
+    1
+);
+
 echo "nome: " . $pessoa->getNomeCompleto();
 echo " senha: " . $pessoa->getSenha();
 echo " email: " . $pessoa->getEmail();
 echo " amnistrador: " . $pessoa->getAdministrado();
 echo " Resumo biografia: " . $pessoa->getResumoBio();
 echo " Fotos: " . $pessoa->getFoto();
+echo "<br>";
+
+echo "titulo: " . $noticia->getTitulo();
+echo " texto dest: " . $noticia->getTextoDestaque();
+echo " textoComple: " . $noticia->getTextoCompleto();
+echo " foto: " . $noticia->getFoto();
+echo " data: " . $noticia->getData();
+echo " iddoaltor: " . $noticia->getIdadminApioadoFK();
+echo "<br>";
 
 $pessoa->setNomeCompleto("Luis");
-
+$noticia->setTitulo("Novo titulo");
+echo "nome:" . $pessoa->getNomeCompleto();
+echo "<br>";
+echo "Titulo: " . $noticia->getTitulo();
