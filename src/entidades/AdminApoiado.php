9fe3dcef164f79844e93c784813a5476a0c0156f
ifeslopes <ifes.lopes@gmail.com>
2021-12-01 11:54:42 -0300

    criando classe adminapiodor e testando set get

---
+++ src/entidades/AdminApoiado.php
@@ -0,0 +1,106 @@
+<?php
+class AdminApoiado
+{
+    private $idadminApioado;
+    private $nomeCompleto;
+    private $email;
+    private $senha;
+    private $administrado;
+    private $resumoBio;
+    private $foto;
+
+    function __construct(
+        $nomeCompleto,
+        $senha,
+        $email,
+        $administrado,
+        $resumoBio,
+        $foto
+    ) {
+        $this->nomeCompleto = $nomeCompleto;
+        $this->senha = $senha;
+        $this->email = $email;
+        $this->administrado = $administrado;
+        $this->resumoBio = $resumoBio;
+        $this->foto = $foto;
+    }
+
+    public function getIdadminApioado()
+    {
+        return $this->idadminApioado;
+    }
+
+    public function getNomeCompleto()
+    {
+        return $this->nomeCompleto;
+    }
+
+    public function setNomeCompleto($nomeCompleto)
+    {
+        $this->nomeCompleto = $nomeCompleto;
+
+        return $this;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+
+        return $this;
+    }
+
+    public function getAdministrado()
+    {
+        return $this->administrado;
+    }
+
+    public function setAdministrado($administrado)
+    {
+        $this->administrado = $administrado;
+
+        return $this;
+    }
+
+    public function getResumoBio()
+    {
+        return $this->resumoBio;
+    }
+
+    public function setResumoBio($resumoBio)
+    {
+        $this->resumoBio = $resumoBio;
+
+        return $this;
+    }
+
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
+
+?>
--- src/index1.php
+++ src/index1.php
@@ -1,3 +1,19 @@
 <?php
-echo "Teste deservidor e commit no Vscode final";
-?>
+include_once "entidades/AdminApoiado.php";
+$pessoa = new AdminApoiado(
+    "leonardo",
+    "1234",
+    "ifes.lopes@gomail.com",
+    1,
+    "estutande de informatica",
+    "teste.jpg"
+);
+echo "nome: " . $pessoa->getNomeCompleto();
+echo " senha: " . $pessoa->getSenha();
+echo " email: " . $pessoa->getEmail();
+echo " amnistrador: " . $pessoa->getAdministrado();
+echo " Resumo biografia: " . $pessoa->getResumoBio();
+echo " Fotos: " . $pessoa->getFoto();
+
+$pessoa->setNomeCompleto("Luis");
+
